9a4d1d824c30c816cc155ca9f8feddc794e2cba9
Glenn Ludszuweit <glenn.ludszuweit@gmail.com>
2021-05-14 16:23:52 +0200

    setup sequelize and mysql

---
+++ users-services/sequelize/migrations/20210514162010-create-userSessions.js
@@ -0,0 +1,47 @@
+module.exports.up = (queryInterface, DataTypes) => {
+  return queryInterface.createTable(
+    'userSessions',
+    {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+      },
+      userId: {
+        allowNull: false,
+        references: {
+          key: 'id',
+          model: 'users',
+        },
+        type: DataTypes.UUID,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: DataTypes.STRING,
+      },
+      passwordHash: {
+        allowNull: false,
+        type: DataTypes.CHAR(64),
+      },
+      expiresAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      charset: 'utf8',
+    }
+  );
+};
+
+module.exports.down = (queryInterface) =>
+  queryInterface.dropTable('userSessions');
